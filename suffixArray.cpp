21f102699917365e7cdce0a05e9b9c46fb7c730d
Thalyson <thalyson.uece@gmail.com>
2020-07-09 18:22:07 -0300

    optimization sort algorithm. Replace the vector<pair<pair<int,int>, int>> for two vector<int>

--- suffixArray.cpp
+++ suffixArray.cpp
@@ -6,22 +6,22 @@ struct SuffixArray{
    string s;
    vector<int> P;
 
-   void mySort(vector< pair< pair<int,int> ,int> > &v){
-      int len = v.size();
+   void mySort(vector< int > &p, vector<int> &level){
 
-      {
-         vector<int> ctn(len);
-         for( auto &it: v ){
-            ctn[ it.first.first ]++;
-         }
-         vector<int> start(len);
-         for(int i = 1; i < len; i++) start[i] = start[i-1]+ctn[i-1];
-         vector< pair< pair<int,int> ,int> > v_new(len);
-         for( auto &it:v) {
-            v_new[ start[ it.first.first ]++ ] = it;
-         }
-         v = v_new;
+      vector<int> ctn(len);
+      for( auto &it: level ){
+         ctn[ it ]++;
       }
+      vector<int> start(len);
+      for(int i = 1; i < len; i++) start[i] = start[i-1]+ctn[i-1];
+      vector< int > p_new(len);
+
+      for( auto &it:p) {
+         p_new[ start[ level[it] ]++ ] = it;
+      }
+
+      p = p_new;
+
    }
 
    SuffixArray(const string _s){
@@ -45,21 +45,19 @@ struct SuffixArray{
       }
 
       for(int k = 0; (1<<(k)) < len; k++){
-         vector< pair< pair<int,int> ,int> > v(len);
 
          for(int i = 0; i < len; i++)
             P[i] = (P[i] - (1<<k) + len)%len;
 
-         for(int i = 0; i < len; i++)
-            v[i] = {{level[ P[i] ], level[  (P[i]+(1<<k))%len ]}, P[i]};
-
-         mySort( v );
-
-         for(int i = 0; i < len; i++) P[i] = v[i].second;
-         level[ P[0] ] = 0;
+         mySort( P, level );
+         vector<int> level_new(len);
+         level_new[ P[0] ] = 0;
          for(int i = 1; i < len; i++){
-            level[ P[i] ] = level[ P[i-1] ] + (v[i].first > v[i-1].first);
+            pair<int,int> prev = {level[P[i-1]], level[ (P[i-1] +(1<<k))%len ] };
+            pair<int,int> now = {level[P[i]], level[ (P[i] +(1<<k))%len ] };
+            level_new[ P[i] ] = level_new[ P[i-1] ] + (now>prev);
          }
+         level = level_new;
       }
    }
 
