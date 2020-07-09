d4810949eaf3c20fc670c613309ba2e4efc6577b
Thalyson <thalyson.uece@gmail.com>
2020-07-09 17:32:32 -0300

    Optimization of sort function

--- suffixArray.cpp
+++ suffixArray.cpp
@@ -6,6 +6,38 @@ struct SuffixArray{
    string s;
    vector<int> P;
 
+   void mySort(vector< pair< pair<int,int> ,int> > &v){
+      int len = v.size();
+
+      {
+         vector<int> ctn(len);
+         for( auto &it: v ){
+            ctn[ it.first.second ]++;
+         }
+         vector<int> start(len);
+         for(int i = 1; i < len; i++) start[i] = start[i-1]+ctn[i-1];
+         vector< pair< pair<int,int> ,int> > v_new(len);
+         for( auto &it:v) {
+            v_new[ start[ it.first.second ]++ ] = it;
+         }
+         v = v_new;
+      }
+
+      {
+         vector<int> ctn(len);
+         for( auto &it: v ){
+            ctn[ it.first.first ]++;
+         }
+         vector<int> start(len);
+         for(int i = 1; i < len; i++) start[i] = start[i-1]+ctn[i-1];
+         vector< pair< pair<int,int> ,int> > v_new(len);
+         for( auto &it:v) {
+            v_new[ start[ it.first.first ]++ ] = it;
+         }
+         v = v_new;
+      }
+   }
+
    SuffixArray(const string _s){
       s = _s+"$";
       len = s.size();
@@ -30,7 +62,8 @@ struct SuffixArray{
          vector< pair< pair<int,int> ,int> > v(len);
          for(int i = 0; i < len; i++)
             v[i] = {{level[i], level[  (i+(1<<k))%len ]}, i};
-         sort( v.begin(), v.end() );
+
+         mySort( v );
 
          for(int i = 0; i < len; i++) P[i] = v[i].second;
          level[ P[0] ] = 0;
