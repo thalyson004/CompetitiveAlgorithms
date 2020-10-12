564c781ce98d35e18113e20c5f30d1d7ed0cf9b9
Thalyson <thalyson.uece@gmail.com>
2020-10-12 19:34:17 -0300

    add firstPossible and lastPossible to binarySearch. These functions work using a possible function by paramenter bool(*)(int).

--- binarysearch.cpp
+++ binarysearch.cpp
@@ -59,16 +59,50 @@ namespace binary{
       }
       return l;
    }
+
+   /* 00000[1]111 */
+   int firstPossible(int l, int r, bool(*possible)(int) ) {
+      while(l<r){
+         int m = (l+r)/2;
+         if( possible(m) ){
+            r = m;
+         }else{
+            l = m+1;
+         }
+      }
+      return l;
+   }
+   /* 1111[1]0000 */
+   int lastPossible(int l, int r, bool(*possible)(int) ) {
+      while(l<r){
+         int m = (l+r+1)/2;
+         if( possible(m) ){
+            l = m;
+         }else{
+            r = m-1;
+         }
+      }
+      return l;
+   }
 }
 //END
 
 //Main
 int N, M, CASES;
+vector<int> v({1,3,4,5,7,8,11,15});
+
+bool func1(int i){
+   return v[i]>9;
+}
+
+bool func2(int i){
+   return v[i]<=4;
+}
 
 main(){
-   vector<int> v({1,3,4,5,7,8,11,15});
-
    cout << binary::contain(2, v) << " " << binary::contain(8, v) << endl;
    cout << binary::firstGreater(2, v) << " " << binary::firstGreater(8, v) << endl;
    cout << binary::firstGreaterOrIgual(2, v) << " " << binary::firstGreaterOrIgual(8, v) << endl;
+   cout << binary::firstPossible(0, v.size()-1, func1) << endl;
+   cout << binary::lastPossible(0, v.size()-1, func2) << endl;
 }
