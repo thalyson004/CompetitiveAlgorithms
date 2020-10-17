b795db91168cbcd913448e7608436d53b9d536a8
Thalyson <thalyson.uece@gmail.com>
2020-10-17 14:37:17 -0300

    topological order and SCC algorithm

--- simpleGraph.cpp
+++ simpleGraph.cpp
@@ -70,6 +70,61 @@ struct SimpleGraph{
       int longest = longestVerticeFrom(0);
       return longestDistanceFrom(longest);
    }
+
+   SimpleGraph transpose(){
+      SimpleGraph ans(vertices);
+      for(int v = 0; v < vertices; v++){
+         for(int u:edges[v])
+            ans.add_edge(u,v);
+      }
+      return ans;
+   }
+
+   void toposort(int v, vector<int> &order, vector<bool> &mark){
+      mark[v]=1;
+      for(int u: edges[v]){
+         if(!mark[u])
+            toposort(u, order, mark);
+      }
+      order.push_back(v);
+   }
+
+   vector<int> toposort(){
+      vector<int> order;
+      vector<bool> mark(vertices);
+      for(int i = 0; i < vertices; i++ ){
+         if(!mark[i])
+            toposort(i, order, mark);
+      }
+      return order;
+   }
+
+   vector<int> scc(){
+      vector<int> comp(vertices);
+      vector<int> order = toposort();
+      SimpleGraph T = transpose();
+
+      int cur = 0;
+      while(order.size()){
+         int v = order.back();
+         order.pop_back();
+         if(comp[v])continue;
+         queue<int> bfs;
+         bfs.push(v);
+         comp[v] = ++cur;
+         while(bfs.size()){
+            v = bfs.front();
+            bfs.pop();
+            for(int u:T[v]){
+               if(!comp[u]){
+                  comp[u]=cur;
+                  bfs.push(u);
+               }
+            }
+         }
+      }
+      return comp;
+   }
 };
 //END
 int main(){
